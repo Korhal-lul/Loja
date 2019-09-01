abc0429c7150cdc51a7cc3392dd6315bcaf78e40
Korhal <ninj4Noob@hotmail.com>
2019-09-01 17:31:36 -0300

    Second Commit

--- src/Controller/Cadastro.java
+++ src/Controller/Cadastro.java
@@ -9,14 +9,15 @@ import Model.Produtos;
 public class Cadastro {
 	public static void cadastra(Produtos i, JComboBox cbbSearch, DefaultTableModel model, JTable tblProdutos) {
 
-		model.setColumnIdentifiers(new String[] { "Produto", "Preco", "Quantidade", "Preco Total" });
+		model.setColumnIdentifiers(new String[] { "ID", "Produto", "Preco", "Quantidade", "Preco Total" });
 
-		Object row[] = new Object[4];
-
-		row[0] = i.getNome();
-		row[1] = ("R$ " + i.getPreco());
-		row[2] = i.getQtd();
-		row[3] = ("R$ " +i.getPreco() * i.getQtd());
+		Object row[] = new Object[5];
+		
+		row[0] = i.getID();
+		row[1] = i.getNome();
+		row[2] = ("R$ " + i.getPreco());
+		row[3] = i.getQtd();
+		row[4] = ("R$ " +i.getPreco() * i.getQtd());
 
 		model.addRow(row);
 
